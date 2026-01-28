e45b2d04f94872ad80c086f45d201e2cd9ad1ab4
Tanisha Kumari <tanishak719@gmail.com>
2026-01-28 23:58:57 +0530

    Add model_helper.py

--- model_helper.py
+++ model_helper.py
@@ -29,10 +29,16 @@ class CarClassifierResNet(nn.Module):
 
 def predict(image_path):
     image = Image.open(image_path).convert("RGB")
+    image = ImageOps.exif_transpose(image)
+
     transform = transforms.Compose([
-        transforms.Resize((224,224)),
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std = [0.229, 0.224, 0.225])
+        transforms.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225]
+        )
     ])
     image_tensor = transform(image).unsqueeze(0)
 
